a0790e6dafb83e6054dab01b6a1df8e623c79ba9
Brandon Richey <brandon.richey@greenhouse.io>
2019-12-10 16:30:59 -0500

    WIP redoing day 3 again?

--- day3/main.go
+++ day3/main.go
@@ -2,48 +2,12 @@ package main
 
 import (
 	"fmt"
-	"math"
 	"strconv"
 )
 
-func findGridSizePerCoords(coords []string) (int, int, int, int) {
-	left, right, top, bottom, cursorX, cursorY := 0, 0, 0, 0, 0, 0
-	for _, coord := range coords {
-		direction, distance := convertCoord(coord)
-		switch direction {
-		case "R":
-			temp := cursorX + distance
-			if temp > right {
-				right = temp
-			}
-			cursorX += distance
-		case "L":
-			temp := cursorX - distance
-			if temp < left {
-				left = temp
-			}
-			cursorX -= distance
-		case "U":
-			temp := cursorY - distance
-			if temp < top {
-				top = temp
-			}
-			cursorY -= distance
-		case "D":
-			temp := cursorY + distance
-			if temp > bottom {
-				bottom = temp
-			}
-			cursorY += distance
-		}
-	}
-	dx := right - left + 1
-	dy := bottom - top + 1
-	sx, sy := int(math.Abs(float64(left))), int(math.Abs(float64(top)))
-	if left < 0 {
-		sx--
-	}
-	return dx, dy, sx, sy
+type point struct {
+	x int
+	y int
 }
 
 func convertCoord(coord string) (string, int) {
@@ -55,96 +19,34 @@ func convertCoord(coord string) (string, int) {
 	return lhs, rhs
 }
 
-func makeGrid(width, height int) [][]int {
-	grid := make([][]int, height)
-	for y := 0; y < height; y++ {
-		grid[y] = make([]int, width)
-	}
-	return grid
-}
-
-func printGrid(input [][]int) {
-	for y := 0; y < len(input); y++ {
-		for x := 0; x < len(input[y]); x++ {
-			if input[y][x] == 0 {
-				fmt.Print(".")
-			} else if input[y][x] == 1 {
-				fmt.Print("#")
-			} else if input[y][x] == 2 {
-				fmt.Print("|")
-			} else if input[y][x] >= 3 {
-				fmt.Print("+")
-			} else if input[y][x] == -1 {
-				fmt.Print("o")
-			} else {
-				fmt.Print("?")
-			}
-		}
-		fmt.Print("\n")
-	}
-}
-
-func startCoords(grid [][]int, inputs []string, sx, sy int) [][]int {
-	nx := sx
-	ny := sy
-	grid[ny][nx] = -1
-	for _, coord := range inputs {
-		grid, nx, ny = drawCoord(grid, coord, nx, ny)
-	}
-	return grid
-}
-
-func drawCoord(input [][]int, coord string, startX int, startY int) ([][]int, int, int) {
-	// Copy the input first
-	gridCopy := makeGrid(len(input[0]), len(input))
-	copy(gridCopy, input)
-	nextX, nextY := 0, 0
-	// Next, parse the coord to get the vector (direction and distance)
-	direction, distance := convertCoord(coord)
-	fmt.Printf("Starting at %d, %d drawing %s for distance %d\n", startX, startY, direction, distance)
+func walkPath(direction string, distance int, start *point) ([]point, point) {
+	start := point{x: 0, y: 0}
+	coords := make([]point, distance)
 	switch direction {
 	case "R":
-		for x := startX + 1; x < len(input[startY]) && x <= (startX+distance); x++ {
-			gridCopy[startY][x] = 1
-			nextX = x
-			nextY = startY
+		for i := 0; i < distance; i++ {
+			coords[i] = point{x: start.x + i, y: start.y}
 		}
 	case "L":
-		for x := startX - 1; x >= 0 && x >= (startX-distance); x-- {
-			gridCopy[startY][x] = 1
-			nextX = x
-			nextY = startY
+		for i := 0; i < distance; i++ {
+			coords[i] = point{x: start.x - i, y: start.y}
 		}
 	case "U":
-		for y := startY - 1; y >= 0 && y >= (startY-distance); y-- {
-			gridCopy[y][startX] = 1
-			nextX = startX
-			nextY = y
+		for i := 0; i < distance; i++ {
+			coords[i] = point{x: start.x, y: start.y - i}
 		}
 	case "D":
-		for y := startY + 1; y < len(input) && y <= (startY+distance); y++ {
-			gridCopy[y][startX] = 1
-			nextX = startX
-			nextY = y
+		for i := 0; i < distance; i++ {
+			coords[i] = point{x: start.x, y: start.y + i}
 		}
 	}
-	return gridCopy, nextX, nextY
+	return nil, coords
 }
 
 func main() {
-	coords := []string{
-		"R8", "U5", "L5", "D3",
+	coords := []string{"R8", "U5", "L5", "D3"}
+	for _, coord := range coords {
+		direction, distance := convertCoord(coord)
+		fmt.Println(walkPath())
 	}
-	w, h, sx, sy := findGridSizePerCoords(coords)
-	fmt.Printf("Grid should be %d X %d\n", w, h)
-	fmt.Printf("And start at %d, %d\n", sx, sy)
-
-	fmt.Println("\n\nMaking the grid")
-	grid := makeGrid(w, h)
-	printGrid(grid)
-
-	fmt.Println("\n\nTime to draw the coords")
-	drawnGrid := startCoords(grid, coords, sx, sy)
-	printGrid(drawnGrid)
-
 }
